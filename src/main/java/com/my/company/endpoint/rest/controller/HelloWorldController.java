31a17ac328016b6021bd45f3001ec751f74f6296
Mihajatiana <154539464+Mihajatiana-dev@users.noreply.github.com>
2025-06-12 07:47:59 +0300

    feat: Create HelloWorldController.java

---
+++ src/main/java/com/my/company/endpoint/rest/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.my.company.endpoint.rest.controller;
+
+import com.my.company.service.HelloWorldService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class HelloWorldController {
+  private final HelloWorldService service;
+
+  @GetMapping("/hello")
+  public String helloWorld(@RequestParam String name) {
+    return service.uploadHelloWorldMessage(name);
+  }
+}
