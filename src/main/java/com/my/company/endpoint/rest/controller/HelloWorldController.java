131b2be22f5e2c7b577270df65c416f9fb6a04a4
Mihajatiana <hei.mihajatiana@gmail.com>
2025-06-12 15:54:41 +0300

    fixup

--- src/main/java/com/my/company/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/HelloWorldController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
+
   private final Mailer mailer;
 
   @GetMapping("/hello")
