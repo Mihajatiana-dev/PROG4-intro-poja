d4adc61bca02d324390040d084ea7a6bcd83d5dc
Mihajatiana <hei.mihajatiana@gmail.com>
2025-06-12 16:38:09 +0300

    fixup

--- src/main/java/com/my/company/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/HelloWorldController.java
@@ -1,8 +1,13 @@
 package com.my.company.endpoint.rest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-@AllArgsConstructor
 public class HelloWorldController {
 
+  @GetMapping("/hello")
+  public String helloWorld() {
     return "... world!";
+  }
 }
