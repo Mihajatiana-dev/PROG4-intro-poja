69efaf196bb0a7416a273aca07850b8511af092a
Mihajatiana <hei.mihajatiana@gmail.com>
2025-06-12 16:41:29 +0300

    fixup

--- src/main/java/com/my/company/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/HelloWorldController.java
@@ -13,11 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
+
   private final Mailer mailer;
 
   @GetMapping("/hello")
   @SneakyThrows
-  public String helloWorld(@RequestParam String to) {
+  public String hello_world(@RequestParam String to) {
     var email =
             new Email(new InternetAddress(to), List.of(), List.of(), "Hello world", "... world!", List.of());
 
