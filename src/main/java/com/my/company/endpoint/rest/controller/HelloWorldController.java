1473aee284961e976ca339981f4be9dc83f64ed4
Mihajatiana <hei.mihajatiana@gmail.com>
2025-06-12 16:15:45 +0300

    fixup

--- src/main/java/com/my/company/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/HelloWorldController.java
@@ -1,38 +1,27 @@
 package com.my.company.endpoint.rest.controller;
 
-import static com.my.company.endpoint.rest.controller.health.PingController.OK;
-
-import com.my.company.PojaGenerated;
 import com.my.company.mail.Email;
 import com.my.company.mail.Mailer;
-import jakarta.mail.internet.AddressException;
 import jakarta.mail.internet.InternetAddress;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@PojaGenerated
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-
-  Mailer mailer;
+  private final Mailer mailer;
 
   @GetMapping("/hello")
-  public ResponseEntity<String> helloWorld(@RequestParam String to) throws AddressException {
+  @SneakyThrows
+  public String helloWorld(@RequestParam String to) {
     var email =
-            new Email(
-                    new InternetAddress(to),
-                    List.of(),
-                    List.of(),
-                    "Hello world",
-                    "... world!",
-                    List.of());
+            new Email(new InternetAddress(to), List.of(), List.of(), "Hello world", "... world!", List.of());
 
     mailer.accept(email);
-    return OK;
+    return "... world!";
   }
 }
